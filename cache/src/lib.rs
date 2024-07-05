47bc01a0922a3189687c6d71a3c106e898899687
edward.ngo <thanh.ngo.2510@gmail.com>
2024-07-06 02:01:56 +0700

    Initial inteface

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::sync::Arc;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+trait ConcurrentCache<K, V> {
+    fn insert(&self, key: K, value: V);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn get(&self, key: &K) -> Arc<&V>;
 }
