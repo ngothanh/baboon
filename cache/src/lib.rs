a8a423ab8bc9521a6a005059472b2c3267c6fe55
edward.ngo <thanh.ngo.2510@gmail.com>
2024-08-05 07:33:10 +0700

    Add simple tests

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -5,7 +5,7 @@ use std::sync::{Arc, RwLock};
 trait ConcurrentCache<K, V> {
     fn insert(&self, key: K, value: V);
 
-    fn get(&self, key: &K) -> Option<Arc<&V>>;
+    fn get(&self, key: &K) -> Option<Arc<V>>;
 }
 
 pub struct LFUCache<K, V>
@@ -15,7 +15,10 @@ where
     main_storage: RwLock<HashMap<K, Arc<V>>>,
 }
 
-impl<K, V> LFUCache<K, V> {
+impl<K, V> LFUCache<K, V>
+where
+    K: Eq + Hash,
+{
     pub fn new(capacity: usize) -> Self {
         Self {
             main_storage: RwLock::new(HashMap::with_capacity(capacity))
@@ -34,12 +37,33 @@ where
         m.insert(key, Arc::new(value));
     }
 
-    fn get(&self, key: &K) -> Option<Arc<&V>> {
+    fn get(&self, key: &K) -> Option<Arc<V>> {
         let m = self.main_storage
             .read()
             .expect("Cannot acquire read lock on the map");
 
         m.get(key)
             .map(|v| Arc::clone(v))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+    use crate::{ConcurrentCache, LFUCache};
+
+    #[test]
+    fn given_newly_insert_kv_when_getting_value_back_then_return_correctly() {
+        //given
+        let key = "test";
+        let value = 10;
+        let cache = LFUCache::new(10);
+        cache.insert(key, value);
+
+        //when
+        let expect = cache.get(&key).unwrap();
+
+        //then
+        assert_eq!(expect, Arc::new(value))
     }
 }
