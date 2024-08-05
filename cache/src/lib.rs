56b4cf0e19959903f9b9e1803cdc918377acafeb
edward.ngo <thanh.ngo.2510@gmail.com>
2024-08-05 07:23:06 +0700

    Add in memory map

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -1,7 +1,45 @@
-use std::sync::Arc;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::{Arc, RwLock};
 
 trait ConcurrentCache<K, V> {
     fn insert(&self, key: K, value: V);
 
-    fn get(&self, key: &K) -> Arc<&V>;
+    fn get(&self, key: &K) -> Option<Arc<&V>>;
+}
+
+pub struct LFUCache<K, V>
+where
+    K: Eq + Hash,
+{
+    main_storage: RwLock<HashMap<K, Arc<V>>>,
+}
+
+impl<K, V> LFUCache<K, V> {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            main_storage: RwLock::new(HashMap::with_capacity(capacity))
+        }
+    }
+}
+
+impl<K, V> ConcurrentCache<K, V> for LFUCache<K, V>
+where
+    K: Eq + Hash,
+{
+    fn insert(&self, key: K, value: V) {
+        let mut m = self.main_storage
+            .write()
+            .expect("Cannot acquire write lock on the map");
+        m.insert(key, Arc::new(value));
+    }
+
+    fn get(&self, key: &K) -> Option<Arc<&V>> {
+        let m = self.main_storage
+            .read()
+            .expect("Cannot acquire read lock on the map");
+
+        m.get(key)
+            .map(|v| Arc::clone(v))
+    }
 }
